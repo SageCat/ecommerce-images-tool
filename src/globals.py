d64d4b7c61cc55b97ef851586d62cf1a4702764d
sage <roachgsq1994@gmail.com>
2024-05-06 23:52:56 +0400

    handle card holder images

--- src/globals.py
+++ src/globals.py
@@ -5,8 +5,8 @@ IMAGES_REPOSITORY_BRANCH = 'main'
 CSV_FILE = '../share_links.csv'
 
 # Products Related
-PRODUCT_IMAGE_FOLDER_PATH = 'images/noon/ToteBag/production'
-PRODUCT_CATEGORY = 'Tote Bag'
+PRODUCT_IMAGE_FOLDER_PATH = 'images/noon/CardHolder/production/Totoro'
+PRODUCT_CATEGORY = 'Card Holder Totoro'
 PRODUCT_PHOTO_NAME = None
 PRODUCT_LINK = None
-SINGLE_PRODUCT_PHOTO_COUNT = 2
+SINGLE_PRODUCT_PHOTO_COUNT = 3
